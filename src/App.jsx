ac295ce067eea35447ac748633fc304af4176a21
DreadfulCrazy <lucasospina97@gmail.com>
2020-04-26 22:08:30 -0500

    Burbling from picker added ans some changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,31 @@ import { fetchData } from './api';
 
 class App extends Component {
 
+    state = {
+        data: {},
+        country:''
+    }
+
     async componentDidMount(){
         const data = await fetchData();
-        console.log(data);
+        this.setState({data});
+    }
+
+    handleCountryChange = async (country) =>{
+        //fetch the data
+        const data = await fetchData(country);
+        //set state
+        this.setState({data, country});
     }
 
     render() {
+        const { data, country } = this.state;
         return (
             <div className = {Styles.container}>
-               <Cards/>
-               <CountryPicker/>
-               <Chart/>
+                <img src="https://i.ibb.co/7QpKsCX/image.png" alt="COBID-19" className ={Styles.image}/>
+               <Cards data= {data}/>
+               <CountryPicker handleCountryChange={this.handleCountryChange}/>
+               <Chart data = {data} country = {country} />
             </div>
         );
     }
